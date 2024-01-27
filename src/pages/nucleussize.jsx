5b036f52b069020c56482263fa35a3ddf2ae822e
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2024-01-27 20:52:48 +0530

    (fix): added image slider for Nuclei Size evaluation

--- src/pages/nucleussize.jsx
+++ src/pages/nucleussize.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 function CellSize() {
     const [imageFile, setImageFile] = useState(null);
@@ -62,27 +63,67 @@ function CellSize() {
                     </button>
                 </form>
 
-                {resultImage && (
-                    <div className="flex flex-col items-center">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
-                            <div>
-                                <p className="text-lg font-semibold mb-2">Original Image:</p>
-                                <img src={originalImage} alt="Original" className="w-3/4 rounded shadow-lg" />
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2">Proceesed Image:</p>
-                                <img src={resultImage} alt="Processed" className="w-3/4 rounded shadow-lg" />
-                            </div>
-                        </div>
-                        <div className="mt-6 text-left">
-                            <p className="text-lg font-semibold mb-2">Total Nuclei: {totalNuclei}</p>
-                            <h3 className="text-lg font-semibold mb-2">Average Nucleus Size</h3>
-                            <p>Top Section: {averageTop} pixels</p>
-                            <p>Middle Section: {averageMiddle} pixels</p>
-                            <p>Bottom Section: {averageBottom} pixels</p>
-                        </div>
+    {resultImage && (
+          <div className="flex flex-col items-center justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
+              <div>
+                <p className="mb-2 text-lg font-semibold">Results</p>
+                <ReactCompareSlider
+                  className="w-full rounded shadow-lg md:w-3/4"
+                  itemOne={
+                    <ReactCompareSliderImage
+                      src={originalImage}
+                      alt="Image one"
+                    />
+                  }
+                  itemTwo={
+                    <ReactCompareSliderImage
+                      src={resultImage}
+                      alt="Image two"
+                    />
+                  }
+                />
+              </div>
+              <div className="grid my-auto text-left md:flex ">
+            
+                <div class="w-full md:w-1/2 cursor-pointer rounded-r-lg border bg-[#f0f0f0] p-2 shadow-none duration-150 hover:shadow-xl">
+                  <h2 class="font-bold text-gray-800 text-2xl text-center mt-5">
+                    Statistics
+                  </h2>
+                  <div class="p-1">
+                    <div class="my-6 flex items-center justify-between p-4 bg-blue-100">
+                      <p class="font-bold text-gray-500">Total Nuclei</p>
+                      <p class="rounded-full bg-blue-500 px-2 py-1 text-xs font-semibold text-white">
+                        {totalNuclei}
+                      </p>
                     </div>
-                )}
+                    <h3 className="mb-2 font-semibold text-gray-500 text-md">
+                      Average Cell Size
+                    </h3>
+                    <div class="my-3 flex items-center justify-between p-4 bg-red-100">
+                      <p class="font-bold text-gray-500">Top Section</p>
+                      <p class="rounded-full bg-red-500 px-2 py-0.5 text-xs font-semibold text-white">
+                        {averageTop} px
+                      </p>
+                    </div>
+                    <div class="my-3 flex items-center justify-between p-4 bg-red-100">
+                      <p class="font-bold text-gray-500">Middle Section</p>
+                      <p class="rounded-full bg-red-500 px-2 py-0.5 text-xs font-semibold text-white">
+                        {averageMiddle} px
+                      </p>
+                    </div>
+                    <div class="my-3 flex items-center justify-between p-4 bg-red-100">
+                      <p class="font-bold text-gray-500">Bottom Section</p>
+                      <p class="rounded-full bg-red-500 px-2 py-0.5 text-xs font-semibold text-white">
+                        {averageBottom} px
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
             </div>
         </div>
     );
